c069f4e361ab9c27e05c7f5f2bb19560397770b1
Eadweard <aurora.coder.zqf@gmail.com>
2026-02-02 14:35:42 +0800

    test

---
+++ main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type TestPerson struct {
+	Name string
+	Age  int
+}
+
+func TestReadReflect(t *testing.T) {
+	var a interface{} = 1
+
+	var p = TestPerson{
+		Name: "John",
+		Age:  20,
+	}
+
+	// aType := reflect.TypeOf(a)
+
+	fmt.Println(reflect.ValueOf(a))
+
+	fmt.Println(reflect.TypeOf(p))
+	fmt.Println(reflect.ValueOf(p))
+}
+
+func TestWriteReflect(t *testing.T) {
+	var a interface{} = map[string]string{
+		"name": "John",
+		"age":  "20",
+	}
+
+	aValue := reflect.ValueOf(&a).Elem()
+	aValue.SetString("name")
+}
